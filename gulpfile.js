6b7a2c2f574da8bc5e6f7d6a2ebd52c7bfb3a8c7
Bruno Rafael Costa <brunorafael.costa@outlook.com>
2021-10-24 09:22:34 +0100

    Base structure

---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+var gulp        = require('gulp'),
+    pug         = require('gulp-pug'),
+    data        = require('gulp-data'),
+    fs          = require('fs');
+    deploy      = require('gulp-gh-pages');
+
+gulp.task('copy_cname', function () {
+    return gulp.src(['./src/CNAME'], 
+                    {base: '.'})
+            .pipe(gulp.dest('dist'))
+    })
+gulp.task('copy_static', function () {
+    return gulp.src(['./src/styles/**/*.*',
+                     './src/fonts/**/*.*',
+                     './src/scripts/**/*.*',
+                     './src/images/**/*.*'],
+                    {base: '.'})
+            .pipe(gulp.dest('dist/static'))
+    })
+
+gulp.task('build_base', function() {
+    return gulp.src('src/index.pug')
+        .pipe(data(function(file) {
+            return JSON.parse(fs.readFileSync('src/data/datasource.pt.json'))
+        }))
+        .pipe(pug())
+        .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('build-pt', function() {
+    return gulp.src('src/views/**/*.pug')
+        .pipe(data(function(file) {
+            return JSON.parse(fs.readFileSync('src/data/datasource.pt.json'))
+        }))
+        .pipe(pug())
+        .pipe(gulp.dest('dist/pt/'));
+});
+
+gulp.task('build-en', function() {
+    return gulp.src('src/views/**/*.pug')
+        .pipe(data(function(file) {
+            return JSON.parse(fs.readFileSync('src/data/datasource.en.json'))
+        }))
+        .pipe(pug())
+        .pipe(gulp.dest('dist/en/'));
+});
+
+gulp.task('build-fr', function() {
+    return gulp.src('src/views/**/*.pug')
+        .pipe(data(function(file) {
+            return JSON.parse(fs.readFileSync('src/data/datasource.fr.json'))
+        }))
+        .pipe(pug())
+        .pipe(gulp.dest('dist/fr/'));
+});
+
+/**
+ * Push build to gh-pages
+ */
+gulp.task('deploy', ['copy_cname', 'copy_static', 'build_base', 'build-pt', 'build-en', 'build-fr'], function () {
+    return gulp.src("./dist/**/*")
+        .pipe(deploy())
+});
