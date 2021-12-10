ee9161d1354b819daafbd8bd53a1255677ad8692
Bruno Rafael Costa <brunorafael.costa@outlook.com>
2021-12-10 13:32:58 +0000

    Update 2021-12-10T13:32:55.677Z

--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -605,3 +605,30 @@ document.addEventListener('gesturestart', function (e) {
 function readyFunctions() {
     initGallery();
 }
+
+function redirectToLanguage(languageBaseUrl, slug) {
+    debugger;
+    var expiresDays = 365;
+    var expires = "";
+    var date = new Date();
+    date.setTime(date.getTime() + (expiresDays*24*60*60*1000));
+    expires = "; expires=" + date.toUTCString();
+    document.cookie = "brunocostagallery.language" + "=" + languageBaseUrl + expires + "; path=/";
+
+    window.location = slug;
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
